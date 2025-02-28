4119ea1e8b79833e9288cf4a0c71ab6163a32466
Brodino <brodino96@gmail.com>
2025-02-28 15:46:54 +0100

    chore: updated description and version feat: added script dependencies

--- src/fxmanifest.lua
+++ src/fxmanifest.lua
@@ -2,10 +2,12 @@ fx_version "cerulean"
 game "gta5"
 
 author "Brodino"
-description "Generic description"
-version "0.1"
+description "A custom in-game item generator for ox_inventory"
+version "1.0"
 
 server_scripts { "dist/server.js" }
 client_scripts { "dist/client.js" }
 
 files { "config.json", "locales/*" }
+
+dependencies { "ox_lib", "ox_inventory" }
